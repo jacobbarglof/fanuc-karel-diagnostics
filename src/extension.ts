cf60be3832de861afc492a3222e29c06142d9ccd
Simon LANGLO <simon.langlo@prodtex.com>
2023-08-28 09:11:08 +0200

    multiple msg

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 import * as os from 'os';
 import * as cp from 'child_process';
+import { disconnect } from 'process';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -34,28 +35,53 @@ function updateDiagnostics(document: vscode.TextDocument, collection: vscode.Dia
 			ktrans = err.stdout.toString();
 		}
 
-		const pattern = /(\n\s*)(?<line>\d+)(\s+)(?<text>.*?)(\n)(.*?\n)(?<message>.*?)(\n)/s;
+		const pattern = /(\n\s*)(?<line>\d+)(\s+)(?<text>.*?)(\n)(.*?\n)(?<message>.*?)(\n)/gs;
 
 		if (ktrans == undefined) return;
-		const m = pattern.exec(ktrans);
-		if (m != null) {
-			if (m.groups == undefined) return;
-			const lineNum = parseInt(m.groups.line);
-			collection.set(document.uri, [{
-				code: m.groups.text,
-				message: m.groups.message,
-				range: new vscode.Range(lineNum - 1, 0, lineNum - 1, 1),
-				severity: vscode.DiagnosticSeverity.Error,
-				source: '',
-				relatedInformation: [
-					new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(1, 8), new vscode.Position(1, 9))), 'first assignment to `x`')
-				]
-			}]);
-		} else {
 
+		let m: RegExpExecArray | null;
+		let problems = 0;
+		const diagnostics: vscode.Diagnostic[] = [];
+		while ((m = pattern.exec(ktrans))) {
+			problems++;
+			if (m != null) {
+				if (m.groups == undefined) return;
+				const lineNum = parseInt(m.groups.line);
+				diagnostics.push({
+					code: m.groups.text,
+					message: m.groups.message,
+					range: new vscode.Range(lineNum - 1, 0, lineNum - 1, 1),
+					severity: vscode.DiagnosticSeverity.Error,
+					source: '',
+					relatedInformation: [
+						new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(1, 8), new vscode.Position(1, 9))), 'first assignment to `x`')
+					]
+				});
+			}
+		}
+		if (problems > 0) {
+			collection.set(document.uri, diagnostics);
+		} else {
 			collection.clear();
 		}
+		// const m = pattern.exec(ktrans);
+		// if (m != null) {
+		// 	if (m.groups == undefined) return;
+		// 	const lineNum = parseInt(m.groups.line);
+		// 	collection.set(document.uri, [{
+		// 		code: m.groups.text,
+		// 		message: m.groups.message,
+		// 		range: new vscode.Range(lineNum - 1, 0, lineNum - 1, 1),
+		// 		severity: vscode.DiagnosticSeverity.Error,
+		// 		source: '',
+		// 		relatedInformation: [
+		// 			new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(1, 8), new vscode.Position(1, 9))), 'first assignment to `x`')
+		// 		]
+		// 	}]);
+		// } else {
 
+		// 	collection.clear();
+		// }
 	} else {
 		collection.clear();
 	}
